c458bf0cd965fb695fd2424d39adc940d567a653
Marzieh Eslami <eslami.marzieh@gmail.com>
2024-03-09 22:16:52 -0800

    Add QuickGrid functionality

--- SchoolSPA/Constants.cs
+++ SchoolSPA/Constants.cs
@@ -3,6 +3,6 @@
 public class Constants
 {
     //public const string SchoolAPIBase = "http://localhost:5208/api/";
-    //public const string SchoolAPIBase = "https://mzefunc.azurewebsites.net/api/";
-    public const string SchoolAPIBase = "/api/";
+    public const string SchoolAPIBase = "https://mzefunc.azurewebsites.net/api/";
+    //public const string SchoolAPIBase = "/api/";
 }
